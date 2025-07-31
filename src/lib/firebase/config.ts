c86c2195809307f2093ec9e57397511c080b3705
edwin de Zak <edwindezak@gmail.com>
2025-07-31 09:22:41 +0000

    sign up failed firebase: error (auth/comfiguration-not-found)

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -8,11 +8,11 @@ const firebaseConfig = {
   projectId: "blueride-landing",
   storageBucket: "blueride-landing.firebasestorage.app",
   messagingSenderId: "80370688267",
-  appId: "1:80370688267:web:859f62f3354b36c17cc20e",
+  appId: "1:80370688267:web:859f62f3354b36c17cc20e"
 };
 
 // Initialize Firebase
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
 export { app, auth };
