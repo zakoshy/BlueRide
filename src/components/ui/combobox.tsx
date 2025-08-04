d95aad01f33e60bc71057e823e9e09d549417ceb
edwin de Zak <edwindezak@gmail.com>
2025-08-04 08:20:09 +0000

    still when i click the input section before typing anything the dropdown

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -54,7 +54,7 @@ export function Combobox({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[--radix-popover-trigger-width] p-0" align="start" side="bottom" sideOffset={4}>
+      <PopoverContent className="w-[--radix-popover-trigger-width] p-0" side="bottom" align="start" sideOffset={4}>
         <Command>
           <CommandInput placeholder={searchPlaceholder} />
           <CommandList>
